506c83cfeec3f0f6baea7931be8f6e2dafe86e6d
Diego Fernando Bastidas Rincon <diego.fernando.bastidas@correounivalle.edu.co>
2019-03-21 09:59:06 -0500

    ventana modal de registro y verificacion del campo Email

--- public/Scripts/Templates/login.js
+++ public/Scripts/Templates/login.js
@@ -1,39 +1,50 @@
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+}
+
 $('#Register').click(function () {
     var email = document.getElementById('user_data_email').value;
     var username = document.getElementById('user_data_username').value;
     var password = document.getElementById('user_data_password').value;
     var Ruta = Routing.generate('RegisterMyUser');
-    $('#exampleModalCenter').modal('hide');
-    swal({
-        title: 'Confirma tu registro',
-        text: '¿Deseas crear una cuenta con la información proporcionada?',
-        type: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Aceptar',
-        cancelButtonText: 'Cancelar',
-        showLoaderOnConfirm: true,
-        focusConfirm: true,
-        allowOutsideClick: false,
-        preConfirm: function () {
-            return new Promise(function () {
-                $.ajax({
-                    type: 'POST',
-                    url: Ruta,
-                    data: ({email:email, username:username, password:password}),
-                    async: true,
-                    dataType : "json",
-                    success: function (data) {
-                        if(data['Validation']==true){
-                            swal("Registrado exitosamente", data['Message'], "success");
-                        }else{
-                            swal("¡Ups!", data['Message'], "error");
+    if(isEmail(email)){
+        $('#exampleModalCenter').modal('hide');
+        swal({
+            title: 'Confirma tu registro',
+            text: '¿Deseas crear una cuenta con la información proporcionada?',
+            type: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Aceptar',
+            cancelButtonText: 'Cancelar',
+            showLoaderOnConfirm: true,
+            focusConfirm: true,
+            allowOutsideClick: false,
+            preConfirm: function () {
+                return new Promise(function () {
+                    $.ajax({
+                        type: 'POST',
+                        url: Ruta,
+                        data: ({email:email, username:username, password:password}),
+                        async: true,
+                        dataType : "json",
+                        success: function (data) {
+                            if(data['Validation']==true){
+                                swal("Registrado exitosamente", data['Message'], "success");
+                            }else{
+                                swal("¡Ups!", data['Message'], "error");
+                            }
                         }
-                    }
+                    })
+                    ;
                 })
-                ;
-            })
-        }
-    });
+            }
+        });
+    }else{
+        swal("¡Ups!", "El email proporcionado no es válido, por favor verifiquelo", "error");
+    }
+
+
 })
 
 
