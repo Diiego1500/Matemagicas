b4c518fbcc219a9624fa729a476b95ab4aee2405
Diego <diego.fernando.bastidas@correounivalle.edu.co>
2019-03-25 13:21:00 -0500

    validacion de Emails

--- src/Form/BlogNotificationType.php
+++ src/Form/BlogNotificationType.php
@@ -4,8 +4,8 @@ namespace App\Form;
 
 use App\Entity\BlogNotification;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +14,7 @@ class BlogNotificationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', TextType::class)
+            ->add('email', EmailType::class)
             ->add('Enviar',SubmitType::class)
         ;
     }
